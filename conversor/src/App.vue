38a5af3e16534b6dbcd838f802e2cf1783144f23
¨marinhomateus¨ <mateus.marinho@hotmail.com.br>
2021-04-20 11:55:38 -0300

    conversor done

--- conversor/src/App.vue
+++ conversor/src/App.vue
@@ -3,7 +3,23 @@
     <h1>Conversor de Moedas</h1>
     <div class="linha">
       <Conversor moeda-a="USD" moeda-b="BRL"/>
+      <h3 class="moeda">DOLAR</h3>
       <Conversor moeda-a="BRL" moeda-b="USD"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda-a="EUR" moeda-b="BRL"/>
+      <h3 class="moeda">EURO</h3>
+      <Conversor moeda-a="BRL" moeda-b="EUR"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda-a="GBP" moeda-b="BRL"/>
+      <h3 class="moeda">LIBRA</h3>
+      <Conversor moeda-a="BRL" moeda-b="GBP"/>
+    </div>
+    <div class="linha">
+      <Conversor moeda-a="CHF" moeda-b="BRL"/>
+      <h3 class="moeda">FRANCO</h3>
+      <Conversor moeda-a="BRL" moeda-b="CHF"/>
     </div>
   </div>
 </template>
@@ -33,7 +49,12 @@ export default {
 .linha {
   display: flex;
   justify-content: space-around;
+  margin-top: 15px;
   }
 
+.moeda{
+  text-align: center;
+  margin: auto;
+}
 
 </style>
